7c38092f20285eab0a2e179288bff4d566ab28c8
mrjeck <mr.jeck_88@mail.ru>
2016-01-25 21:28:17 +0300

    1_commit

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_CS_5._1
+{
+    struct Article
+    { 
+        int productCode;
+        string productName;
+        double productPrice;
+    }
+    struct Client
+    { 
+        int clientCode;
+        string clientFullName;
+        string clientAddress;
+        string clientPhone;
+        string clientNumberOfOrders;//количе-ство заказов осуществленных клиентом
+        string clientTotalAmountOfOrders;//общая сумма заказов клиента
+    }
+    struct RequestItem
+    { 
+        public string Product;
+        public int NumberOfProducts;
+    }
+    struct Request
+    {
+          public Request(int _orderCode, string _clientName, DateTime _orderDate, LinkedList<RequestItem> _listOfOrderedProducts)
+          {
+              orderCode = _orderCode;
+              clientName = _clientName;
+              orderDate = _orderDate;
+              listOfOrderedProducts = _listOfOrderedProducts;
+          }
+          int orderCode;
+          string clientName;
+          DateTime orderDate;
+          LinkedList<RequestItem> listOfOrderedProducts;//перечень заказанных товаров
+          double orderAmount;//сумма заказа (реализовать вычисляемым свойством).
+          double order
+          {
+              get { return orderAmount; }
+              set {                        
+                       foreach(RequestItem req in listOfOrderedProducts)
+                            {
+                                orderAmount += req.Product * req.NumberOfProducts;
+                            }
+                  }
+          }
+    }
+     class Lesson
+    {
+	byte TP;
+    }
+    class Control
+    {
+        LinkedList<Client> Clients;
+        LinkedList<Request> Requests;
+        LinkedList<Article> Articles;
+        Control() { }
+        public void AddNewRequest(int _orderCode, string _clientName, DateTime _orderDate, LinkedList<RequestItem> _listOfOrderedProducts)
+        {
+            Request req;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
